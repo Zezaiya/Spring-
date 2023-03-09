d14664e149a4bb9b2392816997a8c40802225614
Zezai <2754013417@qq.com>
2023-03-09 15:14:30 +0800

    1

--- src/main/java/com/Zezai/核心容器App.java
+++ src/main/java/com/Zezai/核心容器App.java
@@ -1,17 +1,10 @@
 package com.Zezai;
 
-import com.Zezai.dao.Dao;
-import com.Zezai.dao.impl.DaoImpl;
-import com.Zezai.service.Service;
-import com.Zezai.service.impl.ServiceImpl;
-import javafx.application.Application;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import javax.sql.DataSource;
 
-public class App {
+public class 核心容器App {
     public static void main(String[] args) {
         //获取容器
         ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
