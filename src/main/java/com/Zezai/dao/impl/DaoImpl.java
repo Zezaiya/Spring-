b95fe0d5a8f924ec38c32dcf6b441b54d3551a52
Zezai <2754013417@qq.com>
2023-03-09 17:12:27 +0800

    1

--- src/main/java/com/Zezai/dao/impl/DaoImpl.java
+++ src/main/java/com/Zezai/dao/impl/DaoImpl.java
@@ -1,7 +1,10 @@
 package com.Zezai.dao.impl;
 
 import com.Zezai.dao.Dao;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository("DaoImpl")
 public class DaoImpl implements Dao {
     public void daoAction() {
         System.out.println("dao action");
